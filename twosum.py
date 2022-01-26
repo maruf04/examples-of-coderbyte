7dd45388ecd0a1501059908870758f382339e109
marufakan01 <marufakan01@gmail.com>
2022-01-26 21:25:43 +0300

    Added twosum.py

---
+++ twosum.py
@@ -0,0 +1,17 @@
+#coderbyte 0 puan dondu
+def TwoSum(arr):
+      top=[];
+  i=0
+  while(i<len(arr)):
+    j=i+1
+    while(j<len(arr)):
+      if(arr[i] +arr[j]==arr[0]):
+        top.append([arr[i],arr[j]])
+      j=j+1
+    i=i+1
+
+  # code goes here
+  return top
+
+# keep this function call here 
+print TwoSum(raw_input())
